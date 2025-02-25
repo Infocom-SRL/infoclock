aae6f0084f8caa8281350f31a0a070fc87c36b80
Martin <jacobmartin03@hotmail.com>
2025-02-25 19:04:33 +0100

    chore: Update Logo (#2)
    Merge branch 'dev/rewrite' into 'main'
    
    See pull request #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,16 @@ export default defineNuxtConfig({
     host: "127.0.0.1",
   },
 
-  modules: ["@vueuse/nuxt", "@nuxtjs/color-mode", "@nuxt/image", "@nuxt/eslint", "@nuxtjs/tailwindcss", "nuxt-time", "dayjs-nuxt"],
+  modules: [
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/image",
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "nuxt-time",
+    "dayjs-nuxt",
+    "nuxt-svgo"
+  ],
 
   app: {
     head: {
@@ -28,6 +37,11 @@ export default defineNuxtConfig({
   css: ["@/assets/styles/global.css"],
   colorMode: {
     preference: "light",
+  },
+
+  svgo: {
+    autoImportPath: "@/assets/img",
+    componentPrefix: "svg",
   },
 
   dayjs: {
